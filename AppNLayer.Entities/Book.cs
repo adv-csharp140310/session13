0c0f08a8391d28d7873566c6eba89b12e43d75e4
Pashaie <pashaie@gmail.com>
2025-04-21 17:48:37 +0330

    Entitites

---
+++ AppNLayer.Entities/Book.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppNLayer.Entities;
+public class Book 
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(200)]
+    public required string Title { get; set; }
+
+    [MaxLength(100)]
+    public string? Author { get; set; }
+
+    [MaxLength(20)]
+    public string? Isbn { get; set; }
+
+    public int? PublicationYear { get; set; }
+
+    public int CategoryId { get; set; }
+
+    // Navigation Property
+    public virtual Category Category { get; set; } = null!;
+}
---
+++ AppNLayer.Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppNLayer.Entities;
+
+public class Category
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public required string Name { get; set; }
+
+    // Collection Navigation Property
+    public virtual ICollection<Book> Books { get; set; } = new List<Book>();
+}
